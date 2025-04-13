c5b69ab89f16c3104b83dfff2c86d2459cb18779
Irfaan <irfaank778@gmail.com>
2025-04-13 13:34:01 +0530

    socket fixed

--- frontend/dist/assets/index-dDqDyBMO.js
+++ frontend/dist/assets/index-dDqDyBMO.js
@@ -1,4 +1,4 @@
-import{a2 as Q}from"./index-HlxIaKFm.js";import{e as U}from"./index-C73C-Zdm.js";import{s as W}from"./index-Ch_qCilz.js";function V(t){if(t)throw t}/*!
+import{a2 as Q}from"./index-D-2q9Aow.js";import{e as U}from"./index-CrijeGtQ.js";import{s as W}from"./index-Ch_qCilz.js";function V(t){if(t)throw t}/*!
  * Determine if an object is a Buffer
  *
  * @author   Feross Aboukhadijeh <https://feross.org>
--- frontend/utils/socketUtils.ts
+++ frontend/utils/socketUtils.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
 export const createSocketConnection = (): Socket => {
-  const socket = io("http://localhost:4000"); // Ensure this matches your backend URL
+  const socket = io(); // Ensure this matches your backend URL
   return socket;
 };
